ee3a2a32fb69540bea816318b2ef0d5188b74717
Nao-Okada <81859049+Nao-Okada@users.noreply.github.com>
2022-11-05 19:28:29 +0900

    delete ValidationServiceProvider for deploying this to EC2.

--- Search-master/config/app.php
+++ Search-master/config/app.php
@@ -159,7 +159,6 @@ return [
         Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
         Illuminate\Session\SessionServiceProvider::class,
         Illuminate\Translation\TranslationServiceProvider::class,
-        Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
         /*
